6a607cda0cc08cf84b63e7a1c30a97f04436e569
Mohd Asif <asifazhari58@gmail.com>
2025-07-20 21:11:53 +0530

    New project Add

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,38 +2,46 @@ import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.png"
+import Work5 from "../../assets/work5.png";
+import Work6 from "../../assets/Work6.png"
 export const projectsData = [
   {
     id: 1,
+    image: Work6,
+    title: "Resume Analyzer",
+    category: "FullStack",
+    link:"https://github.com/MohdAsif678/Resume-Analyzer"
+  },
+  {
+    id: 2,
     image: Work5,
     title: "Readify Book Store",
     category: "FullStack",
     link:"https://github.com/MohdAsif678/ReadifyBookStore"
   },
   {
-    id: 2,
+    id: 3,
     image: Work1,
     title: "Blood Bank Management System",
     category: "FullStack",
     link:""
   },
   {
-    id: 3,
+    id: 4,
     image: Work2,
     title: "A Resume Analyzer",
     category: "Front-End",
     link:"https://a-resume-analyzer.netlify.app/"
   },
   {
-    id: 4,
+    id: 5,
     image: Work3,
     title: "A Coffee Website",
     category: "Front-End",
     link:"https://asif-coffee-website.netlify.app/"
   },
   {
-    id: 5,
+    id: 6,
     image: Work4,
     title: "Burger Website",
     category: "Front-End",
